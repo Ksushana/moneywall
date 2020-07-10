0c9347a1effde30ebd8c9b614a4164162b24394c
Ksenia Balashova <utyatka@gmail.com>
2020-07-10 20:14:09 +0200

    fix date input

--- build/js/script.js
+++ build/js/script.js
@@ -19044,6 +19044,4 @@ $(function() {
   $("#number").mask("9999 999999",{placeholder:" "});
   $("#code").mask("999-999",{placeholder:" "});
   $("#postcode").mask("999999",{placeholder:" "});
-  $("#issuedate").mask("99/99/9999",{placeholder:"01/01/2001"});
-  $("#birthday").mask("99/99/9999",{placeholder:"01/01/1990"});
 });
