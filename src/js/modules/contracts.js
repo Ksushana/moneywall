3dcbd639ebd3f58663a50d5bc6a7d46e9074d83d
Ksenia Balashova <utyatka@gmail.com>
2020-07-10 17:39:47 +0200

    adaptive

--- src/js/modules/contracts.js
+++ src/js/modules/contracts.js
@@ -8,6 +8,18 @@
   const contracts = welcome.querySelectorAll(`.contract__item`);
   if (contracts.length > 1) {
     welcome.classList.add("old");
+
+    if(window.isMobile()) {
+      $(function () {
+        $("#accordion").accordion({
+          heightStyle: "content",
+          collapsible: true,
+          // active: false
+        });
+      });
+    }
+
+
   };
 
 })();
